e1f01861aecd6157ebe5bc02f732b6ef2ac27994
ivanov6444 <a.ivanov6444@gmail.com>
2023-05-24 14:52:16 +0300

    feat: add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PolygonMapComponent } from './polygon-map/polygon-map.component';
 import { OsmMapComponent } from './osm-map/osm-map.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, PolygonMapComponent, OsmMapComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, LeafletModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatInputModule,
+    LeafletModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
