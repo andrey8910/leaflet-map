ecb52340c8904b25501d5612e36281e87fe9f524
ivanov6444 <a.ivanov6444@gmail.com>
2023-05-24 14:57:43 +0300

    feat(polygon-map): add coords form & addMarker method. Implementation of input of coordinates and choice of marker color in the form + method of add a marker to the map

--- src/app/polygon-map/polygon-map.component.ts
+++ src/app/polygon-map/polygon-map.component.ts
@@ -1,6 +1,8 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import * as L from 'leaflet';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MarkerColor } from '../interfaces/marker-color';
+import { LatLngTuple } from 'leaflet';
 
 @Component({
   selector: 'app-polygon-map',
@@ -8,9 +10,18 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./polygon-map.component.scss'],
 })
 export class PolygonMapComponent implements OnInit {
+  @Input('center') centerCoords: LatLngTuple = [0, 0];
+  @Input('zoom') mapZoom = 1;
   @ViewChild('map', { static: true }) mapEl: ElementRef | undefined;
   @ViewChild('coords', { static: true }) coordsEl: ElementRef | undefined;
   myMap: any;
+
+  markersColorList: MarkerColor[] = [
+    { colorName: 'red', colorValue: '#f70202' },
+    { colorName: 'green', colorValue: '#019421' },
+    { colorName: 'blue', colorValue: '#012394' },
+    { colorName: 'yellow', colorValue: '#f5ec42' },
+  ];
 
   formGroupCoords: FormGroup = this.fb.group({
     latitude: new FormControl('', [
@@ -21,13 +32,26 @@ export class PolygonMapComponent implements OnInit {
       Validators.pattern(/^([+-])?(?:180(?:\.0{1,6})?|((?:|[1-9]|1[0-7])[0-9])(?:\.[0-9]{1,9})?)$/),
       Validators.required,
     ]),
+    markerColor: new FormControl<MarkerColor>(this.markersColorList[0]),
   });
+
+  get latitudeControl(): number {
+    return this.formGroupCoords.controls['latitude'].value;
+  }
+
+  get longitudeControl(): number {
+    return this.formGroupCoords.controls['longitude'].value;
+  }
+
+  get markerColorControl(): MarkerColor {
+    return this.formGroupCoords.controls['markerColor'].value;
+  }
 
   constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {
-    this.myMap = L.map(this.mapEl?.nativeElement).setView([48.5132, 32.2597], 16);
-    const osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  ngOnInit(): void {
+    this.myMap = L.map(this.mapEl?.nativeElement).setView(this.centerCoords, this.mapZoom);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.myMap);
 
@@ -35,6 +59,17 @@ export class PolygonMapComponent implements OnInit {
   }
 
   addMarker(): void {
-    console.log(this.formGroupCoords.value);
+    const markerIcon = L.divIcon({
+      className: 'select-color-marker',
+      iconAnchor: [0, 24],
+      html: `<span style="background-color: ${this.markerColorControl.colorValue}"></span>`,
+    });
+
+    const marker = L.marker([this.latitudeControl, this.longitudeControl], {
+      icon: markerIcon,
+      draggable: true,
+    }).addTo(this.myMap);
+
+    this.myMap.panTo(marker.getLatLng());
   }
 }
