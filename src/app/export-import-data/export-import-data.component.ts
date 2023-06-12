835279411b307aae50bc6a255890ccfd3534db4d
ivanov6444 <a.ivanov6444@gmail.com>
2023-06-12 13:36:04 +0300

    feat(export-import-data): add import file method

--- src/app/export-import-data/export-import-data.component.ts
+++ src/app/export-import-data/export-import-data.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, ElementRef, Input, Renderer2, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, Output, Renderer2, ViewChild } from '@angular/core';
 import { DrawMap, FeatureGroup } from 'leaflet';
 import * as L from 'leaflet';
 
@@ -13,6 +13,9 @@ export class ExportImportDataComponent {
     this.items = value;
     this.ref.markForCheck();
   }
+
+  @Output() fileToImport = new EventEmitter<FeatureGroup>();
+
   @ViewChild('exportBtn', { static: true }) exportBTN: ElementRef | undefined;
   @ViewChild('importBtn', { static: true }) importBTN: ElementRef | undefined;
   get mapItems(): FeatureGroup {
@@ -29,5 +32,17 @@ export class ExportImportDataComponent {
     }
   }
 
-  import() {}
+  import(event: Event) {
+    const files: FileList = <FileList>(<HTMLInputElement>event.target).files;
+    const reader = new FileReader();
+
+    reader.onload = (e: any) => {
+      const target = e.target.result;
+      if (target) {
+        const importFile = JSON.parse(target);
+        this.fileToImport.emit(importFile);
+      }
+    };
+    reader.readAsText(files[0]);
+  }
 }
